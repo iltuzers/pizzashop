52525fc173bb5d409c6f59e66f8431c8cacf1a32
iltuzers <iltuzers@gmail.com>
2023-12-15 10:07:43 -0500

    Register Category, Food, and Ingredient Models on the Admin Site.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Food, Ingredient
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "price", "available"]
+    list_filter = ["available"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name", )}
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["name"]
